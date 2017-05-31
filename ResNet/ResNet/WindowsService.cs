20a9f99d8152cea8d2956726854e82319fa2849b
shoter <damian>
2017-05-31 17:43:57 +0200

    It's working!

---
+++ ResNet/ResNet/WindowsService.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ResNet
+{
+    class WindowsService : ServiceBase
+    {
+        Mutex mutex = new Mutex();
+        Thread mainThread;
+        private System.ComponentModel.IContainer components;
+        bool paused = false;
+        /// <summary>
+        /// Public Constructor for WindowsService.
+        /// - Put all of your Initialization code here.
+        /// </summary>
+        public WindowsService()
+        {
+            this.ServiceName = "ResNet";
+            this.EventLog.Log = "Application";
+
+            // These Flags set whether or not to handle that specific
+            //  type of event. Set to true if you need it, false otherwise.
+            this.CanHandlePowerEvent = true;
+            this.CanHandleSessionChangeEvent = true;
+            this.CanPauseAndContinue = true;
+            this.CanShutdown = true;
+            this.CanStop = true;
+        }
+
+        /// <summary>
+        /// The Main Thread: This is where your Service is Run.
+        /// </summary>
+        static void Main()
+        {
+            Run(new WindowsService());
+        }
+
+        /// <summary>
+        /// Dispose of objects that need it here.
+        /// </summary>
+        /// <param name="disposing">Whether
+        ///    or not disposing is going on.</param>
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        /// OnStart(): Put startup code here
+        ///  - Start threads, get inital data, etc.
+        /// </summary>
+        /// <param name="args"></param>
+        protected override void OnStart(string[] args)
+        {
+            mainThread = new Thread(() =>
+            {
+                Thread.CurrentThread.IsBackground = true;
+                /* run your code here */
+                Program();
+            });
+
+            mainThread.Start();
+            base.OnStart(args);
+        }
+
+        /// <summary>
+        /// OnStop(): Put your stop code here
+        /// - Stop threads, set final data, etc.
+        /// </summary>
+        protected override void OnStop()
+        {
+            mainThread.Abort();
+            base.OnStop();
+        }
+
+        /// <summary>
+        /// OnPause: Put your pause code here
+        /// - Pause working threads, etc.
+        /// </summary>
+        protected override void OnPause()
+        {
+            lock (mutex)
+                paused = true;
+            base.OnPause();
+        }
+
+        /// <summary>
+        /// OnContinue(): Put your continue code here
+        /// - Un-pause working threads, etc.
+        /// </summary>
+        protected override void OnContinue()
+        {
+            lock (mutex)
+                paused = true;
+            base.OnContinue();
+        }
+
+        /// <summary>
+        /// OnShutdown(): Called when the System is shutting down
+        /// - Put code here when you need special handling
+        ///   of code that deals with a system shutdown, such
+        ///   as saving special data before shutdown.
+        /// </summary>
+        protected override void OnShutdown()
+        {
+            mainThread.Abort();
+            base.OnShutdown();
+        }
+
+        /// <summary>
+        /// OnCustomCommand(): If you need to send a command to your
+        ///   service without the need for Remoting or Sockets, use
+        ///   this method to do custom methods.
+        /// </summary>
+        /// <param name="command">Arbitrary Integer between 128 & 256</param>
+        protected override void OnCustomCommand(int command)
+        {
+            //  A custom command can be sent to a service by using this method:
+            //#  int command = 128; //Some Arbitrary number between 128 & 256
+            //#  ServiceController sc = new ServiceController("NameOfService");
+            //#  sc.ExecuteCommand(command);
+
+            base.OnCustomCommand(command);
+        }
+
+        /// <summary>
+        /// OnPowerEvent(): Useful for detecting power status changes,
+        ///   such as going into Suspend mode or Low Battery for laptops.
+        /// </summary>
+        /// <param name="powerStatus">The Power Broadcast Status
+        /// (BatteryLow, Suspend, etc.)</param>
+        protected override bool OnPowerEvent(PowerBroadcastStatus powerStatus)
+        {
+            return base.OnPowerEvent(powerStatus);
+        }
+
+        /// <summary>
+        /// OnSessionChange(): To handle a change event
+        ///   from a Terminal Server session.
+        ///   Useful if you need to determine
+        ///   when a user logs in remotely or logs off,
+        ///   or when someone logs into the console.
+        /// </summary>
+        /// <param name="changeDescription">The Session Change
+        /// Event that occured.</param>
+        protected override void OnSessionChange(
+                  SessionChangeDescription changeDescription)
+        {
+            base.OnSessionChange(changeDescription);
+        }
+
+        public static bool CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    using (var stream = client.OpenRead("http://www.google.com"))
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void Program()
+        {
+            int timeWithoutInternet = 0;
+            int maxTimesWithoutInternet = 5;
+            int cooldown = 60 * 1000;
+            bool isRestarting = false;
+            while (true)
+            {
+
+                if (CheckForInternetConnection() == false)
+                {
+                    if (isRestarting == false)
+                    {
+                        this.EventLog.WriteEntry("No internet connection!", EventLogEntryType.Error);
+                        if (++timeWithoutInternet > maxTimesWithoutInternet)
+                        {
+                            System.Diagnostics.Process.Start("shutdown.exe", "-r -t 300");
+                            isRestarting = true;
+                        }
+                        Thread.Sleep(cooldown);
+                    }
+                }
+                else
+                {
+                    timeWithoutInternet = 0;
+                    if(isRestarting)
+                    {
+                        System.Diagnostics.Process.Start("shutdown.exe", "-a");
+                        isRestarting = false;
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void InitializeComponent()
+        {
+
+        }
+    }
+}
---
+++ ResNet/ResNet/WindowsServiceInstaller.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace ResNet
+{
+    [RunInstaller(true)]
+    public partial class WindowsServiceInstaller : System.Configuration.Install.Installer
+    {
+        public WindowsServiceInstaller()
+        {
+            ServiceProcessInstaller serviceProcessInstaller =
+                               new ServiceProcessInstaller();
+            ServiceInstaller serviceInstaller = new ServiceInstaller();
+
+            //# Service Account Information
+            serviceProcessInstaller.Account = ServiceAccount.LocalSystem;
+            serviceProcessInstaller.Username = null;
+            serviceProcessInstaller.Password = null;
+
+            //# Service Information
+            serviceInstaller.DisplayName = "ResNet";
+            serviceInstaller.StartType = ServiceStartMode.Automatic;
+
+            //# This must be identical to the WindowsService.ServiceBase name
+            //# set in the constructor of WindowsService.cs
+            serviceInstaller.ServiceName = "ResNet";
+
+            this.Installers.Add(serviceProcessInstaller);
+            this.Installers.Add(serviceInstaller);
+        }
+    }
+}
